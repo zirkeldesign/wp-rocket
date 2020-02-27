9e07c59807b95736a8a1a306af2c2d60607bfa9c
Rémy Perona <remperona@gmail.com>
2020-02-27 10:03:39 -0500

    add more constants to boostrap

--- tests/phpstan/bootstrap.php
+++ tests/phpstan/bootstrap.php
@@ -3,4 +3,10 @@
 define( 'DB_NAME', 'local' ); // phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedConstantFound
 define( 'WP_ROCKET_VERSION', '3.5' );
 define( 'WP_ROCKET_SLUG', 'wp_rocket_settings' );
+define( 'WP_ROCKET_PLUGIN_SLUG', 'wp-rocket' );
+define( 'WP_ROCKET_PLUGIN_NAME', 'WP Rocket' );
+define( 'WP_ROCKET_CACHE_PATH', 'wp-content/cache/' );
+define( 'WP_ROCKET_CONFIG_PATH', 'wp-content/wp-rocket-config/' );
+define( 'WP_ROCKET_CACHE_BUSTING_PATH', 'wp-content/cache/busting/' );
+define( 'WP_ROCKET_CACHE_BUSTING_URL', 'http://example.org/wp-content/cache/busting/' );
 
