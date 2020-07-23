1314b2d3c394c0c7282498200215d995ecc57b6b
Rémy Perona <remperona@gmail.com>
2020-07-23 14:05:59 -0400

    add more global constants

--- tests/phpstan/bootstrap.php
+++ tests/phpstan/bootstrap.php
@@ -9,4 +9,6 @@ define( 'WP_ROCKET_CACHE_PATH', 'wp-content/cache/' );
 define( 'WP_ROCKET_CONFIG_PATH', 'wp-content/wp-rocket-config/' );
 define( 'WP_ROCKET_CACHE_BUSTING_PATH', 'wp-content/cache/busting/' );
 define( 'WP_ROCKET_CACHE_BUSTING_URL', 'http://example.org/wp-content/cache/busting/' );
+define( 'WP_ROCKET_WEB_MAIN', 'https://wp-rocket.me/' );
+define( 'WP_ROCKET_ASSETS_IMG_URL', 'http://example.org/wp-content/plugins/wp-rocket/assets/img/' );
 
