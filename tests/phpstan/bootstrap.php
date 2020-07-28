a7c31ed67addfb23224f724d3a5bed50b6784377
Rémy Perona <remperona@gmail.com>
2020-07-28 16:07:04 -0400

    add more constants

--- tests/phpstan/bootstrap.php
+++ tests/phpstan/bootstrap.php
@@ -9,6 +9,10 @@ define( 'WP_ROCKET_CACHE_PATH', 'wp-content/cache/' );
 define( 'WP_ROCKET_CONFIG_PATH', 'wp-content/wp-rocket-config/' );
 define( 'WP_ROCKET_CACHE_BUSTING_PATH', 'wp-content/cache/busting/' );
 define( 'WP_ROCKET_CACHE_BUSTING_URL', 'http://example.org/wp-content/cache/busting/' );
+define( 'WP_ROCKET_MINIFY_CACHE_PATH', 'wp-content/cache/min/' );
+define( 'WP_ROCKET_CRITICAL_CSS_PATH', 'wp-content/cache/critical-css/' ); 
 define( 'WP_ROCKET_WEB_MAIN', 'https://wp-rocket.me/' );
+define( 'WP_ROCKET_ASSETS_URL', 'http://example.org/wp-content/plugins/wp-rocket/assets/' );
 define( 'WP_ROCKET_ASSETS_IMG_URL', 'http://example.org/wp-content/plugins/wp-rocket/assets/img/' );
+
 
