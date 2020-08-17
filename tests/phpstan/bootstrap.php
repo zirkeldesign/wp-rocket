fa9e4f3f83f4f107985c6abec5e172717b07ab63
Rémy Perona <remperona@gmail.com>
2020-08-17 16:30:42 -0400

    update configuration

--- tests/phpstan/bootstrap.php
+++ tests/phpstan/bootstrap.php
@@ -9,10 +9,16 @@ define( 'WP_ROCKET_CACHE_PATH', 'wp-content/cache/' );
 define( 'WP_ROCKET_CONFIG_PATH', 'wp-content/wp-rocket-config/' );
 define( 'WP_ROCKET_CACHE_BUSTING_PATH', 'wp-content/cache/busting/' );
 define( 'WP_ROCKET_CACHE_BUSTING_URL', 'http://example.org/wp-content/cache/busting/' );
+define( 'WP_ROCKET_PATH', 'wp-content/wp-rocket/' );
+define( 'WP_ROCKET_INC_PATH', 'wp-content/wp-rocket/inc/' );
+define( 'WP_ROCKET_FUNCTIONS_PATH', 'wp-content/wp-rocket/inc/functions/' );
+define( 'WP_ROCKET_3RD_PARTY_PATH', 'wp-content/wp-rocket/inc/3rd-party/' );
 define( 'WP_ROCKET_MINIFY_CACHE_PATH', 'wp-content/cache/min/' );
 define( 'WP_ROCKET_CRITICAL_CSS_PATH', 'wp-content/cache/critical-css/' ); 
 define( 'WP_ROCKET_WEB_MAIN', 'https://wp-rocket.me/' );
+define( 'WP_ROCKET_WEB_API', 'https://wp-rocket.me/api/wp-rocket/' );
 define( 'WP_ROCKET_ASSETS_URL', 'http://example.org/wp-content/plugins/wp-rocket/assets/' );
 define( 'WP_ROCKET_ASSETS_IMG_URL', 'http://example.org/wp-content/plugins/wp-rocket/assets/img/' );
+define( 'WP_ROCKET_ASSETS_JS_URL', 'http://example.org/wp-content/plugins/wp-rocket/assets/js/' );
 
 
